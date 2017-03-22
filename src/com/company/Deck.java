4040c39a10c7f7e9e0ad2c4b0c89457c80cbd83e
Derek W Lee <dl447@pvhs-lab112-d08.pvsd.org>
2017-03-22 10:32:48 -0400

    Initial commit

---
+++ src/com/company/Deck.java
@@ -0,0 +1,112 @@
+package com.company;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * The Deck class represents a shuffled deck of cards.
+ * It provides several operations including
+ *      initialize, shuffle, deal, and check if empty.
+ */
+public class Deck {
+
+    /**
+     * cards contains all the cards in the deck.
+     */
+    private List<Card> cards;
+
+    /**
+     * size is the number of not-yet-dealt cards.
+     * Cards are dealt from the top (highest index) down.
+     * The next card to be dealt is at size - 1.
+     */
+    private int size;
+
+    public Deck(String[] ranks, String[] suits, String[] extra){
+        cards = new ArrayList<Card>();
+        for(int i = 0; i < ranks.length; i++){
+            for(int j = 0; j < suits.length; j++) {
+                cards.add(new Card(ranks[i], suits[j]));
+            }
+        }
+        for(int i = 0; i < extra.length; i++){
+            cards.add(new Card(extra[i], "None"));
+        }
+        size = ranks.length * suits.length + extra.length;
+    }
+
+
+    /**
+     * Determines if this deck is empty (no undealt cards).
+     * @return true if this deck is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        if(size == 0)
+            return false;
+        return true;
+    }
+
+    /**
+     * Accesses the number of undealt cards in this deck.
+     * @return the number of undealt cards in this deck.
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Randomly permute the given collection of cards
+     * and reset the size to represent the entire deck.
+     */
+    public void shuffle() {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
+    }
+
+    /**
+     * Deals a card from this deck.
+     * @return the card just dealt, or null if all the cards have been
+     *         previously dealt.
+     */
+    public Card deal() {
+        size = size - 1;
+        return cards.get(size);
+    }
+
+    /**
+     * Generates and returns a string representation of this deck.
+     * @return a string representation of this deck.
+     */
+    @Override
+    public String toString() {
+        String rtn = "size = " + size + "\nUndealt cards: \n";
+
+        for (int k = size - 1; k >= 0; k--) {
+            rtn = rtn + cards.get(k);
+            if (k != 0) {
+                rtn = rtn + ", ";
+            }
+            if ((size - k) % 2 == 0) {
+                // Insert carriage returns so entire deck is visible on console.
+                rtn = rtn + "\n";
+            }
+        }
+
+        rtn = rtn + "\nDealt cards: \n";
+        for (int k = cards.size() - 1; k >= size; k--) {
+            rtn = rtn + cards.get(k);
+            if (k != size) {
+                rtn = rtn + ", ";
+            }
+            if ((k - cards.size()) % 2 == 0) {
+                // Insert carriage returns so entire deck is visible on console.
+                rtn = rtn + "\n";
+            }
+        }
+
+        rtn = rtn + "\n";
+        return rtn;
+    }
+    public void add(Card c){
+        cards.add(c);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,106 @@
+package com.company;
+
+import java.util.*;
+import java.text.DecimalFormat;
+
+/**
+ * This is a class that tests the Deck class.
+ */
+public class Main {
+    private static final String[] numNames = {"",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten"
+    };
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Please input the suits you want.  Type \"Exit\" to go to next input");
+        ArrayList<String> suitsal = new ArrayList<>();
+        while (true) {
+            String s = sc1.next();
+            if (s.equalsIgnoreCase("Exit")) {
+                break;
+            } else {
+                suitsal.add(s);
+            }
+        }
+        System.out.println("What is the lowest and highest non-face card you want in the deck?");
+        int low = sc2.nextInt();
+        int high = sc2.nextInt();
+        System.out.println("Would you like the standard face cards?  (Ace, Jack, King, and Queen.  \"Y\" or \"N\"");
+        boolean f = true;
+        while(true){
+            String face = sc1.next();
+            if(face.equalsIgnoreCase("Y")){
+                f = true;
+                break;
+            }
+            else if(face.equalsIgnoreCase("N")){
+                f = false;
+                break;
+            }
+            else{
+                System.out.println("Please input \"Y\" or \"N\"");
+            }
+        }
+        ArrayList<String> extra = new ArrayList<>();
+        System.out.println("What extra cards would you like?  \"None\" if none.");
+        while(true){
+            String e = sc1.next();
+            if(e.equalsIgnoreCase("None")){
+                break;
+            }
+            else{
+                System.out.println("How many would you like?");
+                int a = sc2.nextInt();
+                for(int i = 0; i < a; i++) {
+                    extra.add(e);
+                }
+            }
+        }
+        String[] suits = new String[suitsal.size()];
+        for(int i = 0; i < suits.length; i++){
+            suits[i] = suitsal.get(i);
+        }
+        int[] values = new int[high - low + 1];
+        for(int i = 0; i < high - low + 1; i++){
+            values[i] = low + i;
+        }
+        int t;
+        if(f){
+            t = 4;
+        }
+        else{
+            t = 0;
+        }
+        String[] ranks = new String[high - low + 1 + t];
+        for(int i = 0; i < values.length; i++){
+            ranks[i] = numNames[values[i]];
+        }
+        if(f){
+            ranks[values.length] = "Ace";
+            ranks[values.length + 1] = "Jack";
+            ranks[values.length + 2] = "King";
+            ranks[values.length + 3] = "Queen";
+        }
+        String[] extras = new String[extra.size()];
+        for(int i = 0; i < extras.length; i++){
+            extras[i] = extra.get(i);
+        }
+        Deck deck = new Deck(ranks, suits, extras);
+        System.out.println(deck.toString());
+
+    }
+}
